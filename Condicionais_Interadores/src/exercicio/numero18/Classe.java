5030376867882d1702d3b0365e631a0b3cdb3d6f
Noturno <Noturno@192.168.7.21>
2017-05-26 20:32:52 -0300

    exercicio atualizado

--- Condicionais_Interadores/src/exercicio/numero18/Classe.java
+++ Condicionais_Interadores/src/exercicio/numero18/Classe.java
@@ -9,31 +9,49 @@ package exercicio.numero18;
 import java.util.Scanner;
 
 public class Classe {
-public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
-	
-	String nome1;
-	int dia1;
-	int mes1;
-	int ano1;
-	String nome2;
-	int dia2;
-	int mes2;
-	int ano2;
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-	System.out.println("Qual o seu nome?");
-	nome1 = scanner.nextLine();
-	System.out.println("Qual o dia do seu aniversário?");
-	dia1 = scanner.nextInt();
-	System.out.println("Qual o mês do seu aniversário?");
-	mes1 = scanner.nextInt();
-	System.out.println("Qual o ano do seu aniversário?");
-	ano1 = scanner.nextInt();
-	
-	String maisvelha = nome1;
-	String maisnova = nome1;
-	
-	
-	
-}
+		String nome1;
+		int dia1;
+		int mes1;
+		int ano1;
+		String nome2;
+		int dia2;
+		int mes2;
+		int ano2;
+
+		System.out.println("Qual o seu nome?");
+		nome1 = scanner.nextLine();
+		System.out.println("Qual o dia do seu aniversário?");
+		dia1 = scanner.nextInt();
+		System.out.println("Qual o mês do seu aniversário?");
+		mes1 = scanner.nextInt();
+		System.out.println("Qual o ano do seu nascimento?");
+		ano1 = scanner.nextInt();
+
+		String maisvelha = nome1;
+		String maisnova = nome1;
+
+		System.out.println("Qual o seu nome?");
+		nome2 = scanner.next();
+		System.out.println("Qual o dia do seu aniversário?");
+		dia2 = scanner.nextInt();
+		System.out.println("Qual o mês do seu aniversário?");
+		mes2 = scanner.nextInt();
+		System.out.println("Qual o ano do seu nascimento?");
+		ano2 = scanner.nextInt();
+		scanner.close();
+
+		if (ano2 < ano1) {
+			maisvelha = nome2;
+		} else if (ano2 == ano1 && mes2 < mes1) {
+			maisvelha = nome2;
+		} else if (ano2 == ano1 && mes2 == mes1 && dia2 < dia1) {
+			maisvelha = nome2;
+		} else {
+			maisnova = nome2;
+		}
+		System.out.println("Mais velha: " + maisvelha + "\r\n" + "Mais nova: " + maisnova);
+	}
 }
